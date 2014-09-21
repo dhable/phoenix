fe268c32d5f7a29dbf90e0c1205072e3e406a1c2
Dan Hable <dhable@gmail.com>
2014-09-20 17:56:01 -0700

    Initial commit of work

---
+++ app.js
@@ -0,0 +1,45 @@
+/*
+ * phoenix: From the ashes the mobile POS will rise.
+ *
+ * Copyright (c) 2014, jetway.io. All rights reserved.
+ */
+var conf = require("./lib/conf"),
+    log = require("./lib/logger")("boot"),
+    pkgMetadata = {version: "0.1.0"};
+
+
+log.info("Bootstrapping jetway.io Phoenix, version %s", pkgMetadata.version);
+log.info("current working directory = %s", process.cwd());
+
+
+var services = [
+   require("./lib/api")
+];
+
+
+services.forEach(function(service) {
+   var start = Date.now();
+
+   try {
+      log.info("attempting start of %s", service.name);
+      service.start(conf);
+   }
+   catch(err) {
+      log.error("failed to start %s [message = %s]", service.name, err.message);
+      // TODO: Emit alarm that service failed to start
+   }
+
+   log.debug("service start execution for %s took %s ms", service.name, Date.now() - start);
+});
+
+
+process.once("SIGINT", function() {
+   services.forEach(function(service) {
+      try {
+         service.stop();
+      }
+      catch(ex) {
+         log.warn("failed to shutdown service %s: %s", service.name, ex);
+      }
+   });
+});
---
+++ gruntfile.js
@@ -0,0 +1,71 @@
+module.exports = function(grunt) {
+  "use strict";
+  require("load-grunt-tasks")(grunt);
+  require("time-grunt")(grunt);
+
+
+  var source = ["*.js", "lib/**/*.js", "spec/**/*.js", "package.json", "conf/*.json"],
+      pkgJSON = grunt.file.readJSON("package.json"),
+      buildVer = pkgJSON.version + (grunt.option("buildNumber") ? "-" + grunt.option("buildNumber") : "");
+  
+  grunt.initConfig({
+    pkg: pkgJSON,
+    ver: buildVer,
+    jshint: {
+      src: source
+    },
+    jasmine_node: {
+      all: ["spec/"]
+    },
+    clean: {
+      testOutput: ["_SpecRunner.html", "*.log"],
+      generatedDocs: ["docs/phoenix"],
+      package: ["dist", "*.tar.gz"]
+    },
+    copy: {
+      app: {
+        files: [
+          {expand: true, src: ["package.json", "app.js"], dest: "dist/"},
+          {expand: true, src: ["lib/**", "conf/**"], dest: "dist/"}
+        ],
+        options: {
+           process: function(content, srcpath) {
+              if(srcpath === "package.json") {
+                 var distPkg = JSON.parse(content);
+                 distPkg.version = buildVer;
+                 return JSON.stringify(distPkg, null, "\t");
+              }
+              else {
+                 return content;
+              }
+           }
+        }
+      }
+    },
+    exec: {
+      dist_npm_install: {
+        cwd: "dist/",
+        cmd: "npm install --production --no-optional"
+      }
+    },
+    compress: {
+      app: {
+        options: {
+          mode: "tgz",
+          pretty: true,
+          archive: "<%= pkg.name %>_<%= ver %>.tar.gz"
+        },
+        files: [
+          {expand: true, cwd: "dist/", src: ["**/*"], dest: "phoenix"}
+        ]
+      }
+    }
+  });
+
+  grunt.registerTask("test", ["jshint", "jasmine_node", "clean:testOutput"]);
+  grunt.registerTask("package", ["copy:app", "exec:dist_npm_install", "compress:app"]);
+  grunt.registerTask("release", ["clean", "test", "package"]);
+
+  // alias default to test since that's most likely what we want to do.
+  grunt.registerTask("default", ["test"]);
+};
