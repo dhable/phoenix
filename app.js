e64d6fa41c6ccf531bae0e49a78608c4ccec3460
Dan Hable <dhable@gmail.com>
2014-11-24 16:45:14 -0800

    Adding license info.
    We're releasing this under GPL v3. Added the LICENCE file pointing to the GPL v3 licence, printed output in the logs when the application boots and also in the header comment in the main file.

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 /*
  * phoenix: From the ashes the mobile POS will rise.
+ * Copyright (C) 2014 Dan Hable & Mark Franklin.
  *
- * Copyright (c) 2014, jetway.io. All rights reserved.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 var path = require("path"),
     _ = require("lodash"),
@@ -11,7 +23,10 @@ var path = require("path"),
     pkgMetadata = {version: "0.1.0"};
 
 
-log.info("Bootstrapping jetway.io Phoenix, version %s", pkgMetadata.version);
+log.info("phoenix, version %s Copyright (C) 2014 Dan Hable & Mark Franklin\n", pkgMetadata.version);
+log.info("This program comes with ABSOLUTELY NO WARRANTY; This is free");
+log.info("software, and you are welcome to redistribute it under certain ");
+log.info("conditions.\n\n");
 log.info("current working directory = %s", process.cwd());
 
 
